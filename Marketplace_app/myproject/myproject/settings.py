375ad17b954ca2212cfc2894953256848a35a080
cupid <sampath.karan975@gmail.com>
2025-10-05 03:02:09 +0530

    allowedhost

--- Marketplace_app/myproject/myproject/settings.py
+++ Marketplace_app/myproject/myproject/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
